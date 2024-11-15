b51aa9788db11b50ed23b38876f4f35810b95eee
Meirkhan <meyrkhan.nyshanov@gmail.com>
2024-11-15 15:26:05 +0500

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Meirkhan Nishonov on 15.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
